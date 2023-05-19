b16f6b5f7b2374953f2cf7b2132e06f974de7b31
박단비 <qkreksqhd@naver.com>
2023-05-19 17:38:09 +0900

    login vue

---
+++ final-pjt-front/src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form action="">
+            <label for="username">USERNAME : </label>
+            <input type="text" id="username" v-model="username"><br><br>
+
+            <label for="password">PASSWORD : </label>
+            <input type="text" id="password" v-model="password"><br><br>
+
+            <label for="first_name">FIRST NAME : </label>
+            <input type="text" id="first_name" v-model="first_name"><br><br>
+
+            <input type="submit" value="LOGIN">
+        </form>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'LogInview',
+    data(){
+        return{
+            username: null,
+            password: null,
+            first_name: null,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
